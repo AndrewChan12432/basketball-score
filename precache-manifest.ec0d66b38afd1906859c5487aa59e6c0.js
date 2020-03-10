aa7c40e2f831f6e84858dec8c4c00bd322821f1a
AndrewChan12432 <55532769+AndrewChan12432@users.noreply.github.com>
2020-03-10 23:32:57 +0800

    Updates

---
+++ precache-manifest.ec0d66b38afd1906859c5487aa59e6c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa263217d0a83b52f7fb490255856c0",
+    "url": "/basketball-score/index.html"
+  },
+  {
+    "revision": "34e26ee730b8df0f2ff1",
+    "url": "/basketball-score/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2a502d7edb5a556ea3a5",
+    "url": "/basketball-score/static/js/2.b32ee55d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/basketball-score/static/js/2.b32ee55d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34e26ee730b8df0f2ff1",
+    "url": "/basketball-score/static/js/main.3d298870.chunk.js"
+  },
+  {
+    "revision": "0d0d2226c7f9be4387a0",
+    "url": "/basketball-score/static/js/runtime-main.e7eb3728.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/basketball-score/static/media/logo.5d5d9eef.svg"
+  }
+]);
