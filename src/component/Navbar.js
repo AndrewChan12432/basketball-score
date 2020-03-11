90e3cb5f5ede747be3140aa6e7abf13e693aee4f
AndrewChan12432 <55532769+AndrewChan12432@users.noreply.github.com>
2020-03-11 16:11:56 +0800

    dev

---
+++ src/component/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="container">
+      <a href="https://www.onlinebah.com/" class="link">
+        JosDrew
+      </a>
+    </div>
+  );
+}
+
+export default Navbar;
