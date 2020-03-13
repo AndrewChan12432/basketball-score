ae8a4d79765e52db02b9a7dfa84b6deec8b0172d
AndrewChan12432 <55532769+AndrewChan12432@users.noreply.github.com>
2020-03-14 02:25:31 +0800

    Updates

---
+++ precache-manifest.3af6f2992504d2c46bc77867dce4c4c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d9b22145c1be2f66fb5ffb8b6b2309",
+    "url": "/basketball-score/index.html"
+  },
+  {
+    "revision": "972deff81b63c5aa76bd",
+    "url": "/basketball-score/static/css/main.c5491caa.chunk.css"
+  },
+  {
+    "revision": "53e2935433862a069298",
+    "url": "/basketball-score/static/js/2.3546a2dc.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/basketball-score/static/js/2.3546a2dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972deff81b63c5aa76bd",
+    "url": "/basketball-score/static/js/main.b041cd9d.chunk.js"
+  },
+  {
+    "revision": "0d0d2226c7f9be4387a0",
+    "url": "/basketball-score/static/js/runtime-main.e7eb3728.js"
+  }
+]);
